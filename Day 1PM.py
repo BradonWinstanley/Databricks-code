233fdb0ef8b3f81452c09fe5c31160abb5be74be
branwin2004@gmail.com <branwin2004@gmail.com>
2023-02-20 14:21:11 +0000

    Added some basic Python

---
+++ Day 1PM.py
@@ -0,0 +1,41 @@
+# Databricks notebook source
+print("hello world")
+
+# COMMAND ----------
+
+# Strings
+First_name = "Brandon"
+Last_name = "Winstanley"
+print(First_name + " " + Last_name)
+
+# Booleans
+bool1 = True
+bool2 = False
+print(bool1)
+
+# Integers
+int1=1
+int2=10
+int3=100
+print(int1+int2)
+
+# Floats
+float1=1.1
+float2=2.2
+float3=3.3
+print (float3-float1)
+
+# Lists
+list1 = ("Bacon", "Tomato", "Sausage", "Egg")
+print (list1)
+
+# COMMAND ----------
+
+number1 = input("please neter input")
+number2 = input ("please enter second number")
+
+print(int(number1) + int(number2))
+
+# COMMAND ----------
+
+
